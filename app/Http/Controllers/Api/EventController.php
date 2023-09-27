0bed5d17d21a87ed13345e09f40a4200e6837424
Marco Giammari <marcogiammari@gmail.com>
2023-09-27 12:24:18 +0200

    optional relation loading

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,11 +14,42 @@ class EventController extends Controller
      */
     public function index()
     {
+        // query comincia a costruire la query
+        $query = Event::query();
+        // array delle relazioni che l'utente può richiedere
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation) {
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn ($q) => $q->with($relation)
+            );
+        }
+
         // Laravel intuisce che deve inviare una response->json()
         // return Event::all();
 
         // raggruppa i risultati nella proprietà "data"
-        return EventResource::collection(Event::with('user')->paginate());
+        return EventResource::collection(
+            $query->latest()->paginate());
+    }
+
+    protected function shouldIncludeRelation(string $relation): bool 
+    {
+        // non è obbligatorio Request $request come parametro per accedere alla request
+        // si può usare la funzione request()
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        /* array_map esegue la funzione trim, per eliminare ogni spazio vuoto, per ogni elemento ritornato dalla funzione explode, che riceve una stringa e genera un array in base a un separatore (la virgola)
+        */
+        $relations = array_map('trim', explode(',', $include));
+
+        // controlla se relation è nell'array relations
+        return in_array($relation, $relations);
     }
 
     /**
