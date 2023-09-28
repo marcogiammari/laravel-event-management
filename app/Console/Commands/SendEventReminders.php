944ad9b68159a9da9b493bf38c7aad7321c3f637
Marco Giammari <marcogiammari@gmail.com>
2023-09-28 21:43:23 +0200

    reminding about events - custom artisan command

---
+++ app/Console/Commands/SendEventReminders.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Event;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class SendEventReminders extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-event-reminders';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sends notifications to all event attendees that event starts soon';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        // with -> nested eager loading
+        $events = Event::with('attendees.user')->whereBetween('start_time', [now(), now()->addDay()])->get();
+
+        $eventCount = $events->count();
+        $eventLabel = Str::plural('event', $eventCount);
+
+        $this->info("Found {$eventCount} {$eventLabel}");
+
+        // each e count sono collection methods
+        // per ogni attendee di ogni evento -> stampa un messaggio con lo user id dell'attendee
+        $events->each(
+            fn ($event) => $event->attendees->each(
+                fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+            )
+        );
+
+        $this->info('Reminder notifications sent successfully');
+    }
+}
