82d522133bc63d6de7049b098ed77c63de9daa2f
Marco Giammari <marcogiammari@gmail.com>
2023-09-29 11:08:06 +0200

    reminding about events - notifications and email sending

--- app/Console/Commands/SendEventReminders.php
+++ app/Console/Commands/SendEventReminders.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Models\Event;
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -39,7 +40,9 @@ class SendEventReminders extends Command
         // per ogni attendee di ogni evento -> stampa un messaggio con lo user id dell'attendee
         $events->each(
             fn ($event) => $event->attendees->each(
-                fn ($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+                fn ($attendee) => $attendee->user->notify(
+                    new EventReminderNotification($event)
+                )
             )
         );
 
